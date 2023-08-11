75b2a91720bb8e382187577e1fc906a670a1f2cb
Maurice Van Wassenhove <mauricevanwassenhove@gmail.com>
2023-08-11 12:22:03 +0200

    test: setup integration testing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use std::io::Error;
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+async fn main() -> Result<(), Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    run(listener)?.await
 }
