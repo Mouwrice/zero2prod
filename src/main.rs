6d8c8dcc6f299ca7797e7989e9f4412d981dda6e
Maurice Van Wassenhove <mauricevanwassenhove@gmail.com>
2023-08-14 15:09:32 +0200

    feat: perform sql query upon subscription added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::io::Error;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -7,8 +8,11 @@ use zero2prod::startup::run;
 async fn main() -> Result<(), Error> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let listener = TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))
         .expect("Failed to bind random port.");
-    run(listener)?.await
+    run(listener, connection)?.await
 }
